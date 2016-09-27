16337c0855c554edd96f78c9e583ecdba952143d
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-09-27 11:26:02 +0100

    add config.local.exs loading in config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -8,31 +8,11 @@ config :pp_api_monitor,
 
   # you get a notification (metrics are checked) every
   # 10*60*1000 milliseconds (that's 10 minutes)
-  every: 10*60*1000
+  every: 60*10*1000
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
-
-# You can configure for your application as:
-#
-#     config :pp_api_monitor, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:pp_api_monitor, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+# As a safer alternative, create a config.local.exs file, and set your config
+# in there (copy the config above and edit the values). That path is
+# gitignored, so it'll never end up being pushed to origin
+if File.exists?("config/config.local.exs") do
+  import_config "config.local.exs"
+end
